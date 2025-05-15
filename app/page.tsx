6ca741ef5eb08b0f1dff2ae453f522c3b3913e7d
Alumist <codealumist@gmail.com>
2025-05-14 19:30:06 -0700

    working home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>HomePage</div>;
+import FeaturtedProducts from '@/components/home/FeaturtedProducts';
+import Hero from '@/components/home/Hero';
+
+function HomePage() {
+  return (
+    <div>
+      <Hero />
+      <FeaturtedProducts />
+    </div>
+  );
 }
+export default HomePage;
