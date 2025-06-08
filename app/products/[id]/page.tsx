cad7de99f45da77646c4de76f01a06b80fec9384
Alumist <codealumist@gmail.com>
2025-06-07 20:56:14 -0700

    ----

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -10,26 +10,29 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
   const dollarsAmount = formatCurrency(price);
+
   return (
     <section>
       <BreadCrumbs name={product.name} />
       <div className='mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
-        {/* image */}
-        <div className='relative h-full '>
+        {/* IMAGE FIRST COL */}
+        <div className='relative h-full'>
           <Image
             src={image}
             alt={name}
             fill
-            sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw'
+            sizes='(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw'
             priority
-            className='w-full rounded-md object-cover'
+            className='w-full rounded object-cover'
           />
         </div>
-        {/* product info section col  */}
+        {/* PRODUCT INFO SECOND COL */}
         <div>
-          <div className='flex gap-x-8 items-center '>
-            <h1 className='capitalize text-3xl font-bold'>{name}</h1>
-            <FavoriteToggleButton productId={params.id} />
+          <div className='flex gap-x-8 items-center'>
+            <h1 className='capitalize text-3xl font-bold'>{name} </h1>
+            <div className='flex items-center gap-x-2'>
+              <FavoriteToggleButton productId={params.id} />
+            </div>
           </div>
           <ProductRating productId={params.id} />
           <h4 className='text-xl mt-2'>{company}</h4>
